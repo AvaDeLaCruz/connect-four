da52c1e11fe7d4b086462e2c2d1b1ba5bb40ab15
Ava DeLaCruz <avad333@gmail.com>
2019-09-15 17:48:04 -0700

    added control flow checking user input and updating hashmap

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 import java.util.HashMap; 
-import java.util.Map;
 
 public class ConnectFour {
 	
-	static final int NUMROWS = 6;
-	static final int NUMCOLS = 7;
-	static char[][] board = new char[NUMROWS][NUMCOLS];
-	static HashMap<Integer, Integer> nextSlotMap = new HashMap<>();
+    public static Scanner scan = new Scanner(System.in);
+	private static final int NUMROWS = 6;
+	private static final int NUMCOLS = 7;
+	private static char[][] board = new char[NUMROWS][NUMCOLS];
+	private static HashMap<Integer, Integer> nextSlotMap = new HashMap<>();
+	private static int currentPlayer = 1;
+	
 	
 //	initialize the hashmap to map each column number to the next available slot (row number)
 //	in the beginning of the game, the first available slot is in the final row (pos NUMROWS-1)
@@ -17,16 +19,18 @@ public class ConnectFour {
 		}
 	}
 	
+//	initialize the connect4 board with all asterisks to represent open slots
 	public static void initializeBoard() {
 		for(int r = 0; r<NUMROWS; r++) {
 			for(int c=0; c<NUMCOLS; c++) {
 				board[r][c] = '*';
 			}
 		}
-		
 	}
 	
+//	print the board, including spaces and pipes
 	public static void printBoard() {
+		System.out.println();
 		for(int r = 0; r<NUMROWS; r++) {
 			System.out.print("|");
 			for(int c=0; c<NUMCOLS; c++) {
@@ -34,16 +38,54 @@ public class ConnectFour {
 			}
 			System.out.println();
 		}
-		
+		System.out.println();
+	}
+	
+//	prompt the current player to select a column
+	public static void promptUser() {
+		System.out.print("Player " + currentPlayer + ", which column would you like to use? ");
+	}
+	
+//	check if the chosen column has open slots left
+	public static boolean colIsValid(int col) {
+//		check if next open slot in given column is a valid slot, i.e. has row value of [0,NUMROWS-1]
+		return col > -1 && col < NUMCOLS && (nextSlotMap.get(col) > -1);
+	}
+	
+//	add a disk to the column the user chose
+	public static void makeMove(int col) {
+		nextSlotMap.replace(col, nextSlotMap.get(col) - 1);
+		System.out.println(nextSlotMap);
+	}
+	
+//	get player's input col value and either make the move or print an error message
+	public static void getMoves() {
+		while(true) { //continue game until force quit
+			promptUser();
+			if(scan.hasNextInt()) { //if user input an int
+				int col = scan.nextInt();
+				if(colIsValid(col)) { //check if valid col value
+					makeMove(col);
+					printBoard();
+					currentPlayer = (currentPlayer == 1) ? 2 : 1;
+				}
+				else { //if invalid int input, print error message
+					System.out.println("Sorry, that is not a valid option.");
+				}
+			}
+			else { //if invalid non-int input, print error message & clear buffer by storing in string
+				System.out.println("Sorry, that is not a valid option.");
+				String invalidInput = scan.next();
+			}
+		}
 	}
 
+	
 	public static void main(String[] args) {
 		initializeHashMap();
 		initializeBoard();
 		printBoard();
-		System.out.println(nextSlotMap);
-		
-		
+		getMoves();	
 	}
 
 }
